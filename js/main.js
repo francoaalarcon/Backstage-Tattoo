324731595f153acae04da731b0ae8bb4518d53b2
francoaalarcon <francoa.alarconbravo@gmail.com>
2022-05-12 13:55:42 -0500

    Web 1.0

---
+++ js/main.js
@@ -0,0 +1,164 @@
+/*  ---------------------------------------------------
+    Template Name: Phozogy
+    Description:  Phozogy photography HTML Template
+    Author: Colorlib
+    Author URI: https://colorlib.com
+    Version: 1.0
+    Created: Colorlib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    // Search model
+    $('.search-switch').on('click', function () {
+        $('.search-model').fadeIn(400);
+    });
+
+    $('.search-close-switch').on('click', function () {
+        $('.search-model').fadeOut(400, function () {
+            $('#search-input').val('');
+        });
+    });
+
+    // Isotppe Filter
+    $(".filter-controls li").on('click', function() {
+        var filterData = $(this).attr("data-filter");
+
+        $(".portfolio-filter, .gallery-filter").isotope({
+            filter: filterData,
+        });
+
+        $(".filter-controls li").removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $(".portfolio-filter, .gallery-filter").isotope({
+        itemSelector: '.pf-item, .gf-item',
+        percentPosition: true,
+        masonry: {
+        // use element for option
+        columnWidth: '.pf-item, .gf-item',
+        horizontalOrder: true,
+      }
+    });
+
+    //Masonary
+    $('.portfolio-details-pic').masonry({
+        itemSelector: '.pdp-item',
+        columnWidth: '.pdp-item'
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*------------------
+        Carousel Slider
+    --------------------*/
+    var hero_s = $(".hs-slider");
+    hero_s.owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        navText: ['<span class="arrow_carrot-left"></span>', '<span class="arrow_carrot-right"></span>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
+    });
+
+    /*------------------
+        Team Slider
+    --------------------*/
+    $(".categories-slider").owlCarousel({
+        loop: true,
+        margin: 20,
+        items: 3,
+        dots: false,
+        nav: true,
+        navText: ['<span class="arrow_carrot-left"></span>', '<span class="arrow_carrot-right"></span>'],
+        stagePadding: 120,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding: 0
+            },
+            768: {
+                items: 2,
+                stagePadding: 0
+            },
+            992: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            }
+        }
+    });
+
+    /*------------------
+        Image Popup
+    --------------------*/
+    $('.image-popup').magnificPopup({
+        type: 'image'
+    });
+
+    /*------------------
+        Video Popup
+    --------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+})(jQuery);
+
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+  
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 200, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 500, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  
+});
